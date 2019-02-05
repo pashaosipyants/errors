3e81c2db0dcfeace79b0b51890167af8541c622c
Pasha <pavelmaxosip@mail.ru>
2019-02-05 05:39:06 +0300

    corrext

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
 /*
 Package errors provides error type for detailed stack-traced description and exception(handle-check) style handling
 
+The traditional error handling idiom in Go is roughly akin to
 
+    if err != nil {
+            return err
+    }
 */
 package errors
